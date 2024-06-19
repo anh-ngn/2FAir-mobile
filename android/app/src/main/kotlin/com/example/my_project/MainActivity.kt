6f93696d3a25f80fd900056897e02cab56614630
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-06-19 04:58:01 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mycompany.fly2fast
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/flutter_flow/custom_icons.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+
+class FFIcons {
+  FFIcons._();
+
+  static const String _icomoonFamily = 'Icomoon';
+
+  // icomoon
+  static const IconData kauthenDog =
+      IconData(0xe900, fontFamily: _icomoonFamily);
+  static const IconData ksteam = IconData(0xeaac, fontFamily: _icomoonFamily);
+  static const IconData ksteam2 = IconData(0xeaad, fontFamily: _icomoonFamily);
+  static const IconData ktux = IconData(0xeabd, fontFamily: _icomoonFamily);
+  static const IconData kandroid = IconData(0xeac0, fontFamily: _icomoonFamily);
+  static const IconData kreddit = IconData(0xeac6, fontFamily: _icomoonFamily);
+  static const IconData kpinterest2 =
+      IconData(0xead2, fontFamily: _icomoonFamily);
+  static const IconData kfirefox = IconData(0xeada, fontFamily: _icomoonFamily);
+}
